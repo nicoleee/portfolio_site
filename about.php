52bc8ce605be593cdb2aa6a71dbe33d8242e3cfc
nicoleee <i7752205@bournemouth.ac.uk>
2014-10-04 17:35:42 +0100

    created about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php 
+require 'php/header.php';
+?>
+<div class="section">
+	<div class="media-holder">
+		<img src="media/header.jpg">
+	</div>
+</div>
+
+
+<?php 
+require 'php/footer.php';
+ ?>
