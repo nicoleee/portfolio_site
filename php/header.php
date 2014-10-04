934bce93d348b965330b060c175544fe7227d770
nicoleee <i7752205@bournemouth.ac.uk>
2014-10-04 17:25:38 +0100

    added nav links

--- php/header.php
+++ php/header.php
@@ -9,11 +9,11 @@
 		<div class="container">
 			<nav class="nav full background--off-white">
 				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Portfolio</a></li>
-					<li><a href="#">CV</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="About.html">About</a></li>
+					<li><a href="Portfolio.html">Portfolio</a></li>
+					<li><a href="Curriculum Vitae.pdf">CV</a></li>
+					<li><a href="Contact.php">Contact</a></li>
 				</ul>
 			</nav>
 		</div>
