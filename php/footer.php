9b062a239ec8c27edce559a45d0d52adc5811787
nicoleee <i7752205@bournemouth.ac.uk>
2014-10-04 17:22:16 +0100

    added social links

--- php/footer.php
+++ php/footer.php
@@ -2,16 +2,16 @@
 		<div class="banner"></div>
 		<ul class="social-links">
 				<li>
-					<a href="#"><img src="media/tumblr.png"></a>
+					<a href="http://dmdnicole.tumblr.com"><img src="media/tumblr.png"></a>
 				</li>
 				<li>
-					<a href="#"><img src="media/linkedin.png"></a>
+					<a href="http://uk.linkedin.com/pub/nicole-cook/60/106/808"><img src="media/linkedin.png"></a>
 				</li>
 				<li>
-					<a href="#"><img src="media/github.png"></a>
+					<a href="https://github.com/nicoleee"><img src="media/github.png"></a>
 				</li>
 				<li>
-					<a href="#"><img src="media/twitter.png"></a>
+					<a href="https://twitter.com/nicole_elocin_"><img src="media/twitter.png"></a>
 				</li>
 		</ul>
 		<p class="align-center copyright"> &copy; Copyright Nicole Cook 2014 | All Rights Reserved</p>
