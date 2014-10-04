1302d36ecace71cfd5cee60cdee32fa99dbccd8a
nicoleee <i7752205@bournemouth.ac.uk>
2014-10-04 17:54:26 +0100

    Added in about me details

--- about.php
+++ about.php
@@ -1,9 +1,24 @@
 <?php 
 require 'php/header.php';
 ?>
-<div class="section">
-	<div class="media-holder">
-		<img src="media/header.jpg">
+<div class="section section--padded push-down">
+	<div class="container">
+		<div class="media-holder">
+			<img src="media/header.jpg">
+		</div>
+	</div>
+</div>
+<div class="section section--padded background--orange">
+	<div class="container constrained">
+		<p class="text--white">My name is Nicole Cook, I originally come from Southampton and currently live in Bournemouth. I am in my final year at Bournemouth University studying (BA Hons) Digital Media Design, I also have some experience in the industry due to undertaking 3 different work placements. These experiences have made me realise my main interest is in graphic and web design, photography, editing and basic web development.</p> 
+
+		<p class="text--white">In my spare time I enjoy watching films, travelling to various locations, driving, sightseeing and attending live music events. I also take an interest in illustration, crafts, make up art and baking.</p> 
+
+		<p class="text--white">I have undertaken a 4 week placement at Zeta Agency in Poole. This experience was hugely valuable, it aided me to develop my skills and discover what I want to do after University. Prior to this I took part in work experience at Cass Productions (November 2011 - December 2011). Cass Productions is a Media Production Company where they create short films, advertisements and music videos for clients. My month of work experience not only allowed me the chance to use software such as Photoshop, Final Cut and Flash but also I had the opportunity to use professional filming equipment.</p> 
+
+		<p class="text--white">My interest in media first began to develop when I studied GCSE Media Studies at Regents Park Community College. I spent a week of work experience at Southampton General Hospital's Media Department. I then went onto follow my interest in A Level education by studying Media Studies, Photography, English Language and AS Level Sociology.
+
+		<p class="text--white">For more information or to contact me, below are links to my blog, LinkedIn, Github and Twitter.</p>
 	</div>
 </div>
 
