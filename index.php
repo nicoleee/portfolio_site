5fb9f9aa495fce4aaeedc31026932aed49f16f48
Paul Bird <paulbird1993@gmail.com>
2015-01-09 18:23:39 +0000

    updated links on the homepage

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require 'php/header.php';
 ?>
 <div class="section section--padded full push-down">
@@ -11,8 +11,8 @@ require 'php/header.php';
 </div>
 <div class="section section--padded full background--orange">
 	<div class="container constrained">
-		<p class="text--white">I'm a digital designer currently studying (BA Hons) Digital media design at Bournemouth University. 
-			My main interest is in digital graphics and web design. 
+		<p class="text--white">I'm a digital designer currently studying (BA Hons) Digital media design at Bournemouth University.
+			My main interest is in digital graphics and web design.
 			I am very familiar with the Adobe suite and have a basic knowledge of HTML and CSS. I also enjoy illustrating
 			and taking and editing photography in my spare time. I originally come from Southampton but live in Bournemouth, Winton
 			in term time.</p>
@@ -21,26 +21,26 @@ require 'php/header.php';
 <div class="section section--padded section--full-height push-down">
 	<div class="container">
 		<div class="third media-holder left">
-			<a href="#"><img src="media/photog.jpg"></a>
+			<a href="photography.php"><img src="media/photog.jpg"></a>
 		</div>
 		<div class="third media-holder left">
-			<a href="#"><img src="media/pencil.jpg"></a>
+			<a href="art.php"><img src="media/pencil.jpg"></a>
 		</div>
 		<div class="third media-holder left">
-			<a href="#"><img src="media/laptop.jpg"></a>
+			<a href="digital-design.php"><img src="media/laptop.jpg"></a>
 		</div>
 		<div class="third media-holder left">
-			<a href="#"><img src="media/about.jpg"></a>
+			<a href="about.php"><img src="media/about.jpg"></a>
 		</div>
 		<div class="third media-holder left">
-			<a href="#"><img src="media/cv.jpg"></a>
+			<a href="media/cv.pdf" target="_blank"><img src="media/cv.jpg"></a>
 		</div>
 		<div class="third media-holder left">
-			<a href="#"><img src="media/contact.jpg"></a>
+			<a href="contact.php"><img src="media/contact.jpg"></a>
 		</div>
 	</div>
 </div>
 
-<?php 
+<?php
 require 'php/footer.php';
  ?>
